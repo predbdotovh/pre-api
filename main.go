87705a8dfc6bef02d2cb61357a147eb71c5129eb
predbdotovh <i@predb.ovh>
2019-07-11 01:05:58 +0200

    Use ENV config with .env autoload instead of .json

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	sphinxDatabase := getEnv("SPHINX_DATABASE", "tcp(127.0.0.1:9306)/?interpolateParams=true")
+	nukesDatabase := getEnv("NUKES_DATABASE", "")
+	listenAddr := getEnv("LISTEN_ADDRESS", "127.0.0.1:8088")
 
-	conf := loadConfig()
-	newSphinx(conf.SphinxDatabase)
-	newMysql(conf.MysqlDatabase)
+	newSphinx(sphinxDatabase)
+	newMysql(nukesDatabase)
 
 	router := newRouter()
 
-	log.Fatal(http.ListenAndServe(conf.Addr, router))
+	log.Fatal(http.ListenAndServe(listenAddr, router))
+}
+
+func getEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		if defaultValue == "" {
+			log.Fatal("Missing mandatory env variable : " + key)
+		}
+
+		return defaultValue
+	}
+
+	return value
 }
